1168939a972e1b656d2fd4b8ae60598121ff0a1d
Luc Casimir <casimir.luc@gmail.com>
2020-10-09 23:19:23 +0200

    = manage files

---
+++ LCFramework/Data/Data.swift
@@ -0,0 +1,18 @@
+//
+//  Data.swift
+//  LCFramework
+//
+//  Created by owee on 08/10/2020.
+//  Copyright © 2020 Devios. All rights reserved.
+//
+
+import Foundation
+
+struct PreviewData {
+    
+    let jackets = ["avatar","avengers","badboys","jurassic","sherlock","spiderman","visiteur"]
+    
+    let movieURLs = ["https://video-ssl.itunes.apple.com/itunes-assets/Video113/v4/e1/b9/5f/e1b95f4a-3148-4ace-a157-7aaa0ecff21c/mzvf_7509515367607243487.640x354.h264lc.U.p.m4v", "https://video-ssl.itunes.apple.com/itunes-assets/Video123/v4/b7/54/59/b754594f-c356-8392-3bad-29a97e1aba59/mzvf_5683344793820660742.640x354.h264lc.U.p.m4v"]
+
+    
+}
